b40a35b00bb7fdf9a8e50bb204f230b78ea08ba3
Vijay Tiwari <vijay.tiwari.vj7>
2020-10-22 20:41:36 +0530

    Updates

---
+++ precache-manifest.ca9cf68a4a7105127503acc10e940e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18ac040bfbd4763fbb1157ce9c98b34",
+    "url": "/anime-finder/index.html"
+  },
+  {
+    "revision": "4241f6c996d413c5c146",
+    "url": "/anime-finder/static/css/main.9377877b.chunk.css"
+  },
+  {
+    "revision": "c7e9a7dac59986ecaef2",
+    "url": "/anime-finder/static/js/2.5208a357.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/anime-finder/static/js/2.5208a357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4241f6c996d413c5c146",
+    "url": "/anime-finder/static/js/main.2b6e052a.chunk.js"
+  },
+  {
+    "revision": "13a3642f3d99ced5a74b",
+    "url": "/anime-finder/static/js/runtime-main.08f7c54e.js"
+  }
+]);
